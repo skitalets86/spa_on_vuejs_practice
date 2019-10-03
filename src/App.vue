25c90b1f77670e269498bbf4c30cb1dfca061272
skitalets <skitalets86@gmail.com>
2019-10-03 15:52:21 +0300

    Added navigation links

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
 <template>
     <v-app>
-        <v-navigation-drawer app temporary>
-            <!-- -->
+        <v-navigation-drawer app temporary v-model="drawer">
+            <v-list>
+                <v-list-item 
+                    v-for="link of links" 
+                    :key="link.title"
+                    :to="link.url">
+                    <v-list-item-icon>
+                        <v-icon>{{ link.icon }}</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                        <v-list-item-title v-text="link.title"></v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
         </v-navigation-drawer>
         <div>
             <v-app-bar app color="deep-purple accent-4" dark>
-                <v-app-bar-nav-icon></v-app-bar-nav-icon>
+                <v-app-bar-nav-icon 
+                    @click="drawer = !drawer"
+                    class="hidden-md-and-up">
+                </v-app-bar-nav-icon>
                 <v-toolbar-title>Ad application</v-toolbar-title>
 
                 <div class="flex-grow-1"></div>
 
-                <v-toolbar-items>
-                    <v-btn text>Link 1</v-btn>
+                <v-toolbar-items class="hidden-sm-and-down">
+                    <v-btn 
+                        v-for="link in links" 
+                        :key="link.title" 
+                        :to="link.url" 
+                        text>
+                        <v-icon left>{{ link.icon }}</v-icon>
+                        {{ link.title }}
+                    </v-btn>
                 </v-toolbar-items>
             </v-app-bar>
         </div>
@@ -23,10 +45,17 @@
 
 <script>
 export default {
-  name: 'App',
-  components: {},
-  data: () => ({
-    //
-  }),
+    name: 'App',
+    components: {},
+    data: () => ({
+        drawer: false,
+        links: [
+            {title: 'Login', icon: 'mdi-lock', url: '/login'},
+            {title: 'Registration', icon: 'mdi-face', url: '/registration'},
+            {title: 'Orders', icon: 'mdi-bookmark-outline', url: '/orders'},
+            {title: 'New ad', icon: 'mdi-file-plus', url: '/new'},
+            {title: 'My ads', icon: 'mdi-format-list-bulleted', url: '/list'}
+        ]
+    }),
 };
 </script>
