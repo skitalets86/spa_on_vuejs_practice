2e864cf1b47766bde84d95fa4e5a6f53388f841d
skitalets <skitalets86@gmail.com>
2019-10-03 13:46:06 +0300

    Added toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,24 @@
 <template>
-  <h1>Hello Vue</h1>
+    <v-app>
+        <v-navigation-drawer app temporary>
+            <!-- -->
+        </v-navigation-drawer>
+        <div>
+            <v-app-bar app color="deep-purple accent-4" dark>
+                <v-app-bar-nav-icon></v-app-bar-nav-icon>
+                <v-toolbar-title>Ad application</v-toolbar-title>
+
+                <div class="flex-grow-1"></div>
+
+                <v-toolbar-items>
+                    <v-btn text>Link 1</v-btn>
+                </v-toolbar-items>
+            </v-app-bar>
+        </div>
+        <v-content>
+            <router-view></router-view>
+        </v-content>
+    </v-app> 
 </template>
 
 <script>
