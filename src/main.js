a330eec9ec7c19f21d3b7b21cde70309e559b264
skitalets <skitalets86@gmail.com>
2019-10-08 14:14:20 +0300

    Added routing to app

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import App from './App'
+import router from './router'
+import vuetify from '@/plugins/vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
 
 Vue.config.productionTip = false
 
+/* eslint-disable no-new */
 new Vue({
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+    vuetify,
+    el: '#app',
+    router,
+    render: h => h(App)
+})
