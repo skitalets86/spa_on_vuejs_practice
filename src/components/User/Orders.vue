552eb36afc6c75b7cee7eeaf2b907a05d9cbe753
skitalets <skitalets86@gmail.com>
2019-10-10 12:12:54 +0300

    Added content for page Orders

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,8 +1,33 @@
 <template>
     <v-container>
         <v-layout row>
-            <v-flex xs12>
-                <h1>Orders</h1>
+            <v-flex xs12 sm6 offset-sm3>
+                <h1 class="text--secondary mb-3">Orders</h1>
+                <v-list subheader two-line flat>
+                    <v-list-item-group multiple>
+                        <v-list-item
+                            v-for="order in orders"
+                            :key="order.id"
+                        >
+                            <v-list-item-action>
+                                <v-checkbox
+                                    :input-value="order.done"
+                                    @change="markDone(order)"
+                                    color="success"
+                                    :disabled="order.done"
+                                ></v-checkbox>
+                            </v-list-item-action>
+
+                            <v-list-item-content>
+                                <v-list-item-title>{{ order.name }}</v-list-item-title>
+                                <v-list-item-subtitle>{{ order.phone }}</v-list-item-subtitle>
+                            </v-list-item-content>
+                            <v-list-item-action>
+                                <v-btn class="primary" :to="'/ad/ + order.adID'">Open</v-btn>
+                            </v-list-item-action>
+                        </v-list-item>
+                    </v-list-item-group>
+                </v-list>
             </v-flex>
         </v-layout>
     </v-container>
@@ -11,7 +36,22 @@
 <script>
 export default {
     data () {
-        return {}
+        return {
+            orders: [
+                {
+                    id: 'frt1',
+                    name: 'Liudmyla',
+                    phone: '380-95-140-45-54',
+                    adID: '123',
+                    done: false
+                }
+            ]
+        }
+    },
+    methods: {
+        markDone(order) {
+            order.done = true
+        }
     }
 }
 </script>
