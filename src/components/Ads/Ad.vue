339971661bfa0f8d7de0af7d9d2a956a3bd3e9ca
skitalets <skitalets86@gmail.com>
2019-10-10 16:16:56 +0300

    Added content to page ad-item

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -2,7 +2,26 @@
     <v-container>
         <v-layout row>
             <v-flex xs12>
-                <h1>Ad</h1>
+                <v-card>
+                    <v-img
+                        src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"
+                        height="300px"
+                    ></v-img>
+                    <v-card-text>
+                        <h1 class="text--primary">Lorem</h1>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem 
+                            quae soluta quam reprehenderit distinctio perferendis ducimus fugit 
+                            eaque quod nihil! Perferendis non maxime iste, tempore nobis saepe 
+                            fuga animi ducimus?
+                        </p>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn class="warning" depressed>Edit</v-btn>
+                        <v-btn class="success">Buy</v-btn>
+                    </v-card-actions>
+                </v-card>
             </v-flex>
         </v-layout>
     </v-container>
